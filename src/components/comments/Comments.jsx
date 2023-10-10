29bd8caa9449417cd4a9922b49ca8eaa5a5482d3
Anuj Agrawal <agrawalanuj8622@gmail.com>
2023-10-10 15:01:35 +0530

    first production

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -24,7 +24,7 @@ const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
   const { data, mutate, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    `https://daily-edventure-git-main-agrawalanuj18.vercel.app/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
