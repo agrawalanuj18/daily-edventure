fa073e8593b84423d987ef602c8141d1eb057c03
Anuj Agrawal <agrawalanuj8622@gmail.com>
2023-10-10 16:50:53 +0530

    adddddddddddddddd

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,23 +1,25 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import dynamic from 'next/dynamic';
+"use client";
 
-// Dynamically import components that might be causing issues with SSR
-const Image = dynamic(() => import('next/image'), { ssr: false });
-const ReactQuill = dynamic(() => import('react-quill'), {
-  ssr: false,
-  loading: () => <p>Loading editor...</p>
-});
-
-let getStorage, ref, uploadBytesResumable, getDownloadURL, app;
-
-if (typeof window !== 'undefined') {
-  ({ getStorage, ref, uploadBytesResumable, getDownloadURL } = require("firebase/storage"));
-  ({ app } = require("@/utils/firebase"));
-}
+import Image from "next/image";
+import styles from "./writePage.module.css";
+import { useEffect, useState } from "react";
+import "react-quill/dist/quill.bubble.css";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "@/utils/firebase";
+import ReactQuill from "react-quill";
 
 const WritePage = () => {
+  const { status } = useSession();
+  const ReactQuill = Dynamic(() => import('react-quill'), {ssr: false})};
   const router = useRouter();
+
   const [open, setOpen] = useState(false);
   const [file, setFile] = useState(null);
   const [media, setMedia] = useState("");
@@ -26,38 +28,47 @@ const WritePage = () => {
   const [catSlug, setCatSlug] = useState("");
 
   useEffect(() => {
-    if (file && getStorage && ref && uploadBytesResumable && getDownloadURL) {
-      const storage = getStorage(app);
-      const upload = () => {
-        const name = new Date().getTime() + file.name;
-        const storageRef = ref(storage, name);
-        const uploadTask = uploadBytesResumable(storageRef, file);
+    const storage = getStorage(app);
+    const upload = () => {
+      const name = new Date().getTime() + file.name;
+      const storageRef = ref(storage, name);
 
-        uploadTask.on(
-          "state_changed",
-          (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log("Upload is " + progress + "% done");
-            switch (snapshot.state) {
-              case "paused":
-                console.log("Upload is paused");
-                break;
-              case "running":
-                console.log("Upload is running");
-                break;
-            }
-          },
-          (error) => {},
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              setMedia(downloadURL);
-            });
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
           }
-        );
-      };
-      upload();
-    }
+        },
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setMedia(downloadURL);
+          });
+        }
+      );
+    };
+
+    file && upload();
   }, [file]);
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "unauthenticated") {
+    router.push("/");
+  }
 
   const slugify = (str) =>
     str
@@ -86,14 +97,14 @@ const WritePage = () => {
   };
 
   return (
-    <div>
-      {/* Your component JSX here */}
+    <div className={styles.container}>
       <input
         type="text"
         placeholder="Title"
+        className={styles.input}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <select onChange={(e) => setCatSlug(e.target.value)}>
+      <select className={styles.select} onChange={(e) => setCatSlug(e.target.value)}>
         <option value="style">style</option>
         <option value="fashion">fashion</option>
         <option value="food">food</option>
@@ -101,39 +112,40 @@ const WritePage = () => {
         <option value="travel">travel</option>
         <option value="coding">coding</option>
       </select>
-      <div>
-        <button onClick={() => setOpen(!open)}>
+      <div className={styles.editor}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
-          <div>
+          <div className={styles.add}>
             <input
               type="file"
               id="image"
               onChange={(e) => setFile(e.target.files[0])}
               style={{ display: "none" }}
             />
-            <button>
+            <button className={styles.addButton}>
               <label htmlFor="image">
                 <Image src="/image.png" alt="" width={16} height={16} />
               </label>
             </button>
-            <button>
+            <button className={styles.addButton}>
               <Image src="/external.png" alt="" width={16} height={16} />
             </button>
-            <button>
+            <button className={styles.addButton}>
               <Image src="/video.png" alt="" width={16} height={16} />
             </button>
           </div>
         )}
         <ReactQuill
+          className={styles.textArea}
           theme="bubble"
           value={value}
           onChange={setValue}
           placeholder="Tell your story..."
         />
       </div>
-      <button onClick={handleSubmit}>
+      <button className={styles.publish} onClick={handleSubmit}>
         Publish
       </button>
     </div>
