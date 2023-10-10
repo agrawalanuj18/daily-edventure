8cf20d12e56888e58bd4f7c03093ff72884315ae
Anuj Agrawal <agrawalanuj8622@gmail.com>
2023-10-10 15:41:19 +0530

    addddddddd

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
 import styles from "./writePage.module.css";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router"; // Fixed the import path for useRouter
+import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
-import { app } from "@/utils/firebase";
-
-// Dynamically import ReactQuill to avoid SSR issues
 import dynamic from 'next/dynamic';
+
+// Dynamically import Firebase and ReactQuill to avoid SSR issues
 const ReactQuill = dynamic(() => import('react-quill'), {
   ssr: false,
   loading: () => <p>Loading editor...</p>
 });
+
+let getStorage, ref, uploadBytesResumable, getDownloadURL, app;
+
+if (typeof window !== 'undefined') {
+  ({ getStorage, ref, uploadBytesResumable, getDownloadURL } = require("firebase/storage"));
+  ({ app } = require("@/utils/firebase"));
+}
 
 const WritePage = () => {
   const { status } = useSession();
@@ -30,38 +30,40 @@ const WritePage = () => {
   const [catSlug, setCatSlug] = useState("");
 
   useEffect(() => {
-    const storage = getStorage(app);
-    const upload = () => {
-      const name = new Date().getTime() + file.name;
-      const storageRef = ref(storage, name);
+    if (typeof window !== 'undefined' && file) {
+      const storage = getStorage(app);
+      const upload = () => {
+        const name = new Date().getTime() + file.name;
+        const storageRef = ref(storage, name);
 
-      const uploadTask = uploadBytesResumable(storageRef, file);
+        const uploadTask = uploadBytesResumable(storageRef, file);
 
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {},
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setMedia(downloadURL);
+            });
           }
-        },
-        (error) => {},
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setMedia(downloadURL);
-          });
-        }
-      );
-    };
+        );
+      };
 
-    file && upload();
+      upload();
+    }
   }, [file]);
 
   if (status === "loading") {
@@ -88,7 +90,7 @@ const WritePage = () => {
         desc: value,
         img: media,
         slug: slugify(title),
-        catSlug: catSlug || "style", //If not selected, choose the general category
+        catSlug: catSlug || "style",
       }),
     });
 
@@ -154,4 +156,9 @@ const WritePage = () => {
   );
 };
 
-export default WritePage;
+const DynamicWritePage = dynamic(() => Promise.resolve(WritePage), {
+  ssr: false,
+  loading: () => <p>Loading...</p>
+});
+
+export default DynamicWritePage;
