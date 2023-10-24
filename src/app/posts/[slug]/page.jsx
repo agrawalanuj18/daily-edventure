a81181cf27815db0327a59c22cad590ba44b5a21
Anuj Agrawal <agrawalanuj8622@gmail.com>
2023-10-24 18:21:17 +0530

    changes

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 
 const getData = async (slug) => {
-  const res = await fetch(`https://dailyedventure.com//api/posts/${slug}`, {
+  const res = await fetch(`https://dailyedventure.com/api/posts/${slug}`, {
     cache: "no-store",
   });
 
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -6,7 +6,7 @@ import Card from "../card/Card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `https://dailyedventure.com//api/posts?page=${page}&cat=${cat || ""}`,
+    `https://dailyedventure.com/api/posts?page=${page}&cat=${cat || ""}`,
     {
       cache: "no-store",
     }
